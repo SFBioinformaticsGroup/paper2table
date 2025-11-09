77f0f767473f9dd085665a2787b712876770ad23
Franco Leonardo Bulgarelli <franco@mumuki.org>
2025-11-09 03:02:41 -0300

    introduce table schema for pdfplumber

--- src/paper2table/readers/pdfplumber.py
+++ src/paper2table/readers/pdfplumber.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Optional
 
 import pandas as pd
 import pdfplumber
@@ -13,12 +14,24 @@ _logger = logging.getLogger(__name__)
 
 type TableFragment = list[list[str | None]]
 
-
-def parse_column_names_hints(hints: str) -> list[str]:
-    return [normalize_name(hint) for hint in tokenize_schema(hints)]
+type ColumnMappings = dict[int, str]
 
 
-def read_tables(pdf_path: str, column_names_hints: str) -> TablesProtocol:
+class TableSchema:
+    title: str
+    first_page: int
+    last_page: int
+    column_mappings: ColumnMappings
+
+
+class TablesSchema:
+    tables: list[TableSchema]
+    citation: str
+
+
+def read_tables(
+    pdf_path: str, column_names_hints: str, schema: Optional[TablesSchema]
+) -> TablesProtocol:
     try:
         pdf = pdfplumber.open(pdf_path)
     except Exception as e:
@@ -26,20 +39,41 @@ def read_tables(pdf_path: str, column_names_hints: str) -> TablesProtocol:
         return Tables(pdf_path, [])
 
     tables = []
-    parsed_hints = parse_column_names_hints(column_names_hints)
-    for page in pdf.pages:
-        try:
-            table_fragments = page.extract_tables()
-            for table_fragment in table_fragments:
-                dataframe = read_table(
-                    table_fragment if table_fragment else [],
-                    column_names_hints=parsed_hints,
+    if schema:
+        for table_schema in schema.tables:
+            for page in range(table_schema.first_page, table_schema.last_page + 1):
+                table_fragment = pdf.pages[page].extract_tables()[-1]
+                try:
+                    dataframe = read_table(
+                        table_fragment if table_fragment else [],
+                        column_mappings=table_schema.column_mappings,
+                    )
+                    tables.append(
+                        Table(title=table_schema.title, page=page, dataframe=dataframe)
+                    )
+                except Exception as e:
+                    _logger.warning(f"Error reading page {page} of {pdf_path}: {e}")
+    else:
+        parsed_hints = parse_column_names_hints(column_names_hints)
+        for page in pdf.pages:
+            try:
+                table_fragments = page.extract_tables()
+                for table_fragment in table_fragments:
+                    dataframe = read_table(
+                        table_fragment if table_fragment else [],
+                        column_names_hints=parsed_hints,
+                    )
+                    tables.append(Table(page.page_number, dataframe))
+            except Exception as e:
+                _logger.warning(
+                    f"Error reading page {page.page_number} of {pdf_path}: {e}"
                 )
-                tables.append(Table(page.page_number, dataframe))
-        except Exception as e:
-            _logger.warning(f"Error reading page {page.page_number} of {pdf_path}: {e}")
 
     return Tables(pdf_path, tables)
+
+
+def parse_column_names_hints(hints: str) -> list[str]:
+    return [normalize_name(hint) for hint in tokenize_schema(hints)]
 
 
 def first_row_is_table_header(rows: TableFragment, column_names_hints: list[str]):
@@ -58,9 +92,15 @@ def to_dataframe(rows: TableFragment, column_names_hints: list[str]):
 
 
 def read_table(
-    table_fragment: TableFragment, column_names_hints: list[str]
+    table_fragment: TableFragment,
+    column_names_hints: list[str],
+    column_mappings=Optional[ColumnMappings],
 ) -> pd.DataFrame:
     dataframe = to_dataframe(table_fragment, column_names_hints)
+
+    if column_mappings is not None:
+        dataframe = dataframe[column_mappings.keys()].rename(column_mappings)
+
     dataframe = dataframe.rename(columns=lambda column: normalize_name(str(column)))
     dataframe = dataframe.apply(
         lambda row: list(
--- src/paper2table/tables.py
+++ src/paper2table/tables.py
@@ -1,16 +1,19 @@
 import os
+from typing import Optional
 from pandas import DataFrame
 
 
 class Table:
-    def __init__(self, page: int, dataframe: DataFrame):
+    def __init__(self, page: int, dataframe: DataFrame, title: Optional[str] = None):
         self.page = page
         self.df = dataframe
+        self.title = title
 
     def to_dict(self):
         return {
             "rows": self.df.to_dict(orient="records"),
             "page": self.page,
+            **({"title": self.title} if self.title else {}),
         }
 
 
