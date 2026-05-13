9bf82a5ac564ea635dd74d1e37aa7e294f7d4418
Franco Leonardo Bulgarelli <franco@mumuki.org>
2026-05-13 01:40:46 -0300

    Dont write empty files

--- src/paper2table/writers/file.py
+++ src/paper2table/writers/file.py
@@ -1,13 +1,21 @@
 import json
 import os
+import logging
 
 from paper2table.tables_reader import TablesReader
 
+_logger = logging.getLogger("pape2table")
 
-def write_tables(tables: TablesReader, paper_path: str, output_directory: str):
+
+def write_tables(reader: TablesReader, paper_path: str, output_directory: str):
     tables_path = os.path.join(
         output_directory,
         os.path.basename(paper_path).replace(".pdf", ".tables.json"),
     )
+
+    if not reader.tables:
+        _logger.warning(f"no tables could be extracted from {paper_path}")
+        return
+
     with open(tables_path, "w", encoding="utf-8") as f:
-        f.write(json.dumps(tables.to_dict(), ensure_ascii=False))
+        f.write(json.dumps(reader.to_dict(), ensure_ascii=False))
